4b313312d6b41f3229a066196a961d3e5de4b9fb
Srujan Nageshreddy Pundrawar <srujanpundrawar2005@gmail.com>
2026-06-05 19:31:22 +0530

    Time: 31 ms (6.26%), Space: 152.2 MB (5.74%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,38 @@
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+class MinStack {
+    stack<int> mainStack;
+    stack<int> minStack;
+
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        mainStack.push(val);
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.top());
+        }
+    }
+    
+    void pop() {
+        
+        if (!mainStack.empty()) {
+            mainStack.pop();
+            minStack.pop();
+        }
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
