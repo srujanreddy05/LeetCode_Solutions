9ca51b71539fb359a82b37c49fa6153d431bab2b
Pundrawar Srujan Nageshreddy <srujanpundrawar2005@gmail.com>
2026-07-24 18:39:36 +0530

    Time: 12 ms (29.03%), Space: 46.8 MB (44.1%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -11,25 +11,21 @@
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-        vector<int> v;
-        ListNode* curr = head;
-        while(curr != nullptr){
-            int greater = 0;
-            ListNode* temp = curr->next;
-
-            while(temp != nullptr){
-                if(temp->val > curr->val){
-                    greater = temp->val;
-                    break;
-                }
-                temp = temp->next;
-            }
-            v.push_back(greater);
-            curr = curr->next;
+        vector <int> v;
+        while(head != nullptr){
+            v.push_back(head->val);
+            head = head->next;
         }
-       vector<int> result(v.size());
-        for(int i = 0; i < v.size(); i++)
-        result[i] = v[i];
-        return result;
+        int n = v.size();
+        vector<int> ans(n,0);
+        stack <int> st;
+        for(int i = n - 1; i >= 0; i--){
+           while(!st.empty() && st.top() <= v[i])
+            st.pop();
+            if(!st.empty())
+            ans[i] = st.top();
+            st.push(v[i]);
+        }
+        return ans;
     }
 };
